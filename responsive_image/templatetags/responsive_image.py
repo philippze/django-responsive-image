388d3353ef568ef42674287b8ea63a74600a35c7
Philipp Zedler <philipp@neue-musik.com>
2016-04-27 16:14:45 +0200

    new concept

--- responsive_image/templatetags/responsive_image.py
+++ responsive_image/templatetags/responsive_image.py
@@ -1,9 +1,14 @@
 import math
 
+from classytags.arguments import Argument, Flag
+from classytags.core import Options
 from easy_thumbnails.files import get_thumbnailer
+from sekizai.helpers import get_varname
+from sekizai.templatetags.sekizai_tags import SekizaiTag, SekizaiParser
 
 from django import template
 from django.conf import settings
+from django.template import Context, Template
 from django.templatetags.static import static
 
 register = template.Library()
@@ -12,6 +17,37 @@ register = template.Library()
 MIN_SIZE = getattr(settings, 'RESPONSIVE_MIN_SIZE', '360x360')
 MAX_SIZE = getattr(settings, 'RESPONSIVE_MAX_SIZE', '2000x2000')
 
+HTML = """
+    <div class="responsive-image">
+        <img src="{{ src }}" />
+        <div class="responsive-image__background responsive-image__background-{{ count }}"></div>
+    </div>
+"""
+
+STYLE = """
+@media(min-width: {{ width }}px) {
+    .responsive-image__background-{{ count }} {
+        background-image: url('{{ url }}');
+    }
+}
+"""
+
+GLOBAL_STYLE = """
+.responsive-image {
+    position: relative;
+}
+.responsive-image img {
+    width: 100%;
+}
+.responsive-image__background {
+    background-size: 100% 100%;
+    bottom: 0;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+}
+"""
 
 class ResponsiveImageException(Exception):
     pass
@@ -87,22 +123,6 @@ class ResponsiveImage(object):
     def __init__(self, image, ratio):
         self.thumbnailer = get_thumbnailer(image)
         self.base_size = Size(ratio)
-        self.factors = self.get_factors()
-        self.sizes = self.get_sizes()
-
-    ##
-
-    def get_factors(self):
-        min_factor = Size(MIN_SIZE) / self.base_size
-        return [min_factor]
-
-    def get_sizes(self):
-        sizes = []
-        for factor in self.factors:
-            sizes.append(
-                factor * self.base_size
-            )
-        return sizes
 
     # Image operations
 
@@ -113,11 +133,21 @@ class ResponsiveImage(object):
         }
         return self.thumbnailer.get_thumbnail(options)
 
+    def for_width(self, width):
+        factor = width / self.base_size.x
+        if width % self.base_size.x > 0:
+            factor += 1
+        options = {
+            'size': list(factor * self.base_size),
+            'crop': True
+        }
+        return self.thumbnailer.get_thumbnail(options)
+
     # Output
 
     def src(self):
-        thumbnail = self.get_thumbnail(self.sizes[0])
-        return 'src="%s"' % thumbnail.url
+        thumbnail = self.get_thumbnail(self.base_size)
+        return thumbnail.url
 
 
 @register.simple_tag
@@ -130,3 +160,49 @@ def responsive_image_js():
     return '<script src="%s"></script>' % static(
         'responsive_image/responsive_image.js'
     )
+
+class ResponsiveImageTag(SekizaiTag):
+    name = 'responsive_image'
+
+    options = Options(
+        Argument('image'),
+        Argument('ratio'),
+        parser_class=SekizaiParser,
+    )
+
+    def render_tag(self, context, image, ratio, nodelist):
+        self.image = ResponsiveImage(image, ratio)
+        varname = get_varname()
+        count = context[varname].get('responsive-img-count', '1')
+        try:
+            count = int(count)
+        except TypeError:
+            count = 1
+        self.remember(context, count)
+        return self.html(image, ratio, count)
+
+    def remember(self, context, count):
+        #print (count)
+        #print (count.__class__)
+        name = 'css'
+        varname = get_varname()
+        context[varname]['responsive-img-count'] = '%s' % (count + 1)
+        style_tag = self.style(300, count)
+        context[varname][name].append(style_tag)
+        if count == '1':
+            context[varname][name].append('<style>%s</style>' % GLOBAL_STYLE)
+
+    def html(self, image, ratio, count):
+        src = self.image.src()
+        template = Template(HTML)
+        context = Context({'src': src, 'count': count})
+        return template.render(context)
+
+    def style(self, width, count):
+        thumbnail = self.image.for_width(width)
+        url = thumbnail.url
+        style = Template(STYLE)
+        context = Context({'width': 300, 'count': count, 'url': url})
+        return '<style>%s</style>' % style.render(context)
+
+register.tag('responsive_image', ResponsiveImageTag)
